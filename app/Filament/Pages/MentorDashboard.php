cbab311520dbd0230bddf9b81f93e1a6d1d9122c
= <=>
2026-07-01 23:31:44 +0700

    update

--- app/Filament/Pages/MentorDashboard.php
+++ app/Filament/Pages/MentorDashboard.php
@@ -9,11 +9,11 @@ use App\Livewire\MentorMenteesProgressWidget;
 
 class MentorDashboard extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-presentation-chart-line';
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-presentation-chart-line';
     protected static ?string $navigationLabel = 'Mentor Dashboard';
-    protected static ?string $title = 'Mentor Dashboard';
+    protected static string | \Illuminate\Contracts\Support\Htmlable | null $title = 'Mentor Dashboard';
     protected static ?int $navigationSort = 1;
-    protected static ?string $navigationGroup = 'Mentoring';
+    protected static string | \UnitEnum | null $navigationGroup = 'Mentoring';
 
     public static function canAccess(): bool
     {
