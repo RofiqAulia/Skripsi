3151c9aee3eb3e96104770716c5e46a78ecaa4bb
= <=>
2026-07-04 00:27:13 +0700

    update

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -24,11 +24,46 @@ class UserForm
                     ->columnSpanFull(),
                 TextInput::make('position'),
                 TextInput::make('company'),
-                \Filament\Forms\Components\Select::make('department_id')
-                    ->label('Department / Placement')
-                    ->relationship('department', 'name')
-                    ->searchable()
-                    ->preload(),
+                \Filament\Schemas\Components\Section::make('Placement')
+                    ->description('Pilih HANYA SALAH SATU penempatan (Department, Group, ATAU Direktorat).')
+                    ->schema([
+                        \Filament\Forms\Components\Select::make('department_id')
+                            ->label('Department')
+                            ->relationship('department', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                if ($state) {
+                                    $set('group_id', null);
+                                    $set('direktorat_id', null);
+                                }
+                            }),
+                        \Filament\Forms\Components\Select::make('group_id')
+                            ->label('Group')
+                            ->relationship('group', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                if ($state) {
+                                    $set('department_id', null);
+                                    $set('direktorat_id', null);
+                                }
+                            }),
+                        \Filament\Forms\Components\Select::make('direktorat_id')
+                            ->label('Direktorat')
+                            ->relationship('direktorat', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                if ($state) {
+                                    $set('department_id', null);
+                                    $set('group_id', null);
+                                }
+                            }),
+                    ])->columns(3),
                 TextInput::make('name'),
                 TextInput::make('email')
                     ->label('Email address')
