64ba0f49917d52fbfb6d37bd606dc28935043f06
= <=>
2026-07-04 17:52:13 +0700

    update

--- resources/views/study-progress-report/create.blade.php
+++ resources/views/study-progress-report/create.blade.php
@@ -758,4 +758,16 @@
         tbody.insertAdjacentHTML('beforeend', html);
     }
 </script>
+
+<!-- jQuery and Select2 JS -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.select2').select2({
+            theme: 'bootstrap-5',
+            width: '100%'
+        });
+    });
+</script>
 @endsection
